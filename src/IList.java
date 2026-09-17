a6c11117f345af8374e62b6e910e114fef3bec87
Laura García Miguel <lgarciamiguel93@gmail.com>
2026-09-16 23:40:53 -0400

    Se implemento la interface IList

--- src/IList.java
+++ src/IList.java
@@ -1,2 +1,15 @@
-public class IList {
+public interface IList<E> {
+    void add(E e);
+
+    void add(E e, int index);
+
+    E remove(int index);
+
+    E get(int index);
+
+    int size();
+
+    void clear();
+
+    boolean isEmpty();
 }
