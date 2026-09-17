bfcbcff731722f968ae678dd43b9d5cc167ea546
Laura García Miguel <lgarciamiguel93@gmail.com>
2026-09-17 18:13:09 -0400

    Implementacion de Node

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,31 @@
-public class Node {
+public class Node<T> {
+    //un nodo contiene 2 parametros, valor y un puntero al siguiente elemento
+    protected T val;
+    protected Node<T> next;
+
+    public Node(T val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    public Node(T val, Node<T> next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public T getVal() {
+        return val;
+    }
+
+    public void setVal(T val) {
+        this.val = val;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
 }
